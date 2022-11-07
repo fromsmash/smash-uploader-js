f334ad2242c85bf2e642e27269ea82335efdc3f6
terry <terry@fromsmash.com>
2022-11-07 16:35:47 +0100

    added rollup plugin and bump version

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import builtins from 'rollup-plugin-node-builtins';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
 import globals from 'rollup-plugin-node-globals';
 import { terser } from 'rollup-plugin-terser';
 import json from '@rollup/plugin-json';
@@ -22,6 +23,7 @@ export default [
             }),
             commonjs(),
             globals(),
+            nodePolyfills(),
         ],
     },
     {
